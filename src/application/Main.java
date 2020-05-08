538a809c0b25df7f33f597e70e68a1c41f25f8de
JoseRHeras <jrheras@yahoo.com>
2020-05-08 11:21:35 -0500

    Small chagnes

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,14 +3,32 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 
 
 public class Main extends Application {
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			
+			
+			GridPane root = new GridPane();
+			
+			TextField text1 = new TextField("0");
+			TextField text2 = new TextField("0");
+			text1.setPrefWidth(30.0);
+			text2.setPrefWidth(30.0);
+			
+			root.add(text1, 0, 0);
+			root.add(text2, 1, 0);
+			
+			
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
@@ -23,4 +41,6 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
+
+
 }
